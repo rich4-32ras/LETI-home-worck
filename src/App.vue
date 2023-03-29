fd2ba123ec0754c96dbe196e2acb9f8060053b5c
rich4-32ras <semyon.shilo@yandex.ru>
2023-03-29 17:58:59 +0300

    hw2 v-1

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import css from './App.module.css'
+import { ref } from 'vue';
+
+const  text = ref("");
+const cards = ref([{msg: "деревня", inf: "за городом"}, {msg: "деревяшки", inf: "из них строят дома"}, {msg: "дом", inf: "можно жить и есть"}]);
+
+</script>
+
+<template>
+    <h1 :class="css.myClass" >//HW2//</h1>
+    <input :class="css.myInput" v-model="text"/>
+
+    <template  v-for="card in cards">
+      
+      <div :class="css.myRectangleCard" v-if="card.msg.includes(text)">
+        <h2>{{ card.msg }}</h2>
+        <p>{{ card.inf }}</p>
+      </div>
+    
+    </template>
+</template>
